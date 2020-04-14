8a658e2c3c6c8fe61660bbc30a56006e3e37983a
JanP <janek.piekarski.postfach@gmail.com>
2020-04-14 10:27:33 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/SucukiFarmer/EasyDraw""
    This reverts commit 696e56928929f80525ca2d8ac84f30608b3e8b6d.

---
+++ src/core/gui/component/Logic.java
@@ -0,0 +1,6 @@
+package core.gui.component;
+
+public interface Logic<T>
+{
+	public void exec(T target);
+}
---
+++ src/core/gui/component/logic/ButtonLogic.java
@@ -0,0 +1,8 @@
+package core.gui.component.logic;
+
+import core.gui.component.EDButton;
+
+public interface ButtonLogic
+{
+	public void exec(EDButton target);
+}
