a1adb5c5c686cab61616ab7482e872a520e55b1d
JanP <janek.piekarski.postfach@gmail.com>
2020-08-09 15:35:25 +0200

    Fixed invalid font path

--- src/core/feature/Path.java
+++ src/core/feature/Path.java
@@ -4,9 +4,9 @@ import java.io.File;
 
 public interface Path
 {
-	public static final String CORE_PATH = new File(System.getProperty("user.dir") + "\\src").exists()
-			? (System.getProperty("user.dir") + "\\src")
-			: (System.getProperty("user.dir") + "\\bin");
+	public static final String CORE_PATH = new File(System.getProperty("user.dir") + "\\src\\core").exists()
+			? (System.getProperty("user.dir") + "\\src\\core")
+			: (System.getProperty("user.dir") + "\\bin\\core");
 
-	public static final String CORE_MEDIA = Path.CORE_PATH + "\\media";
+	public static final String FONTS = Path.CORE_PATH + "\\gui\\font\\image";
 }
